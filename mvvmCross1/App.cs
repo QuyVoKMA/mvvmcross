0b8121c5d759dabf2d20c338444a507999d9f426
Quy Vo <vovanquykma.96@gmail.com>
2021-07-21 17:26:04 +0700

    first commit

---
+++ mvvmCross1/App.cs
@@ -0,0 +1,18 @@
+﻿using MvvmCross.ViewModels;
+using mvvmCross1.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvmCross1
+{
+   public class App: MvxApplication
+    {
+        public override void Initialize()
+        {
+            RegisterAppStart<GuestBookViewModel>();
+        }
+    }
+}
---
+++ mvvmCross1/ViewModels/GuestBookViewModel.cs
@@ -0,0 +1,38 @@
+﻿using MvvmCross.ViewModels;
+using mvvmCross1.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mvvmCross1.ViewModels
+{
+   public class GuestBookViewModel: MvxViewModel
+    {
+        // ObservableCollection nó mô tả một tập hợp các dữ liệu có thể thay đổi
+        private ObservableCollection<PersonModel> _people = new ObservableCollection<PersonModel>();
+
+        public ObservableCollection<PersonModel> People
+        {
+            get { return _people; }
+            set { SetProperty(ref _people , value); }
+        }
+
+
+        private string _firstName;
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { SetProperty(ref _firstName, value); }
+        }
+
+        private string _lastName;
+        public string LastName
+        {
+            get { return _lastName; }
+            set { SetProperty(ref _lastName, value); }
+        }
+    }
+}
